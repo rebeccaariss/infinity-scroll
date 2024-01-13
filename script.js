7da84ad60da87c2a478488af96048e57b57e1725
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-01-12 22:24:31 -0500

    Add async function for accessing Unsplash API

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ import config from './config';
 const count = 10;
 const apiKey = config.apiKey;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Error handling here
+  }
+}
+
+// On load
+getPhotos();
